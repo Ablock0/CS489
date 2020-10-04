0877d387cabe2dde1dddf99bea765323fa8a5360
AJ Block <anthony.block@wsu.edu>
2020-10-03 20:34:35 -0700

    adding all code

---
+++ StarterCodes/Extra Stater I think/client/src/components/CoursesPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+
+       render() {
+        return (
+        <div className="padded-page center">
+            <h1>Courses</h1>
+            <h2>This page is under construction.</h2>
+            <p style={{fontStyle: "italic"}}>
+            CptS 489 React<br/></p>
+        </div>
+        );
+    }   
+}
+
+export default CoursesPage;
